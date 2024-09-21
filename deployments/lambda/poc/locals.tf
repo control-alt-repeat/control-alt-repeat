e1264a82f0351c0e1bc8282b1e5113226fe76cd3
Matt Canty <matthewcanty@gmail.com>
2024-09-21 14:06:06 +0000

    Fix locals, hopefully

--- deployments/lambda/poc/locals.tf
+++ deployments/lambda/poc/locals.tf
@@ -1,8 +1,8 @@
 locals {
   function_name = "poc"
-  src_path      = "${path.module}/../cmd/aws/lambda/poc/lambda/${local.function_name}"
+  src_path      = "${path.module}/../../../cmd/aws/lambda/${local.function_name}"
 
   binary_name  = local.function_name
-  binary_path  = "${path.module}/../cmd/aws/lambda/poc/tf_generated/${local.binary_name}"
-  archive_path = "${path.module}/../cmd/aws/lambda/poc/tf_generated/${local.function_name}.zip"
+  binary_path  = "${local.src_path}/tf_generated/${local.binary_name}"
+  archive_path = "${local.src_path}/tf_generated/${local.function_name}.zip"
 }
