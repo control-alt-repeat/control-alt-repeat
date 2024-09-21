89726eefa102a5b6b541693ef16028e165689b78
Matt Canty <matthewcanty@gmail.com>
2024-09-21 17:20:36 +0000

    Add v1 deployment files

---
+++ deployments/v1/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    archive = {
+      source = "hashicorp/archive"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+  }
+
+  required_version = ">= 1.9.6"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      github_repository = "control-alt-repeat"
+      github_organisation = "Control-Alt-Repeat"
+      stage_name = var.stage_name
+    }
+  }
+}
+
+module "lambda_poc" {
+    source = "./lambda/poc"
+}
