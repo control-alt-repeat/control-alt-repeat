7a96ab3bec8b341ee91ad14de32974e1dfd0aa94
Matt Canty <matthewcanty@gmail.com>
2024-09-21 17:34:07 +0000

    Move aws provider to config directory

--- configs/test/main.tf
+++ configs/test/main.tf
@@ -20,3 +20,31 @@ module "terraform_state_backend" {
 #   source = "./../../deployments/v1"
 #   stage_name = reverse(split("/", path.cwd))[0]
 # }
+
+provider "aws" {
+  region = "eu-west-2"
+
+  default_tags {
+    tags = {
+      github_repository = "control-alt-repeat"
+      github_organisation = "Control-Alt-Repeat"
+      stage_name = "test"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    archive = {
+      source = "hashicorp/archive"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+  }
+
+  required_version = ">= 1.9.6"
+}
