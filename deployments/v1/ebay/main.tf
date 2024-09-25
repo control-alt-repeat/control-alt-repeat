0c85b8ddc121a3a2c06f0a30d0b5ff5bb24e068d
Matt Canty <matthewcanty@gmail.com>
2024-09-25 15:37:05 +0000

    Reference fix

--- deployments/v1/ebay/main.tf
+++ deployments/v1/ebay/main.tf
@@ -5,8 +5,8 @@ module "auth" {
 module "lambda_import_listing" {
     source = "./lambda/import-listing"
 
-    read_listings_policy_arn = aws_iam_policy.function_read_listings.arn
-    write_listings_policy_arn = aws_iam_policy.function_write_listings.arn
+    read_listings_policy_arn = aws_iam_policy.allow_read_listings.arn
+    write_listings_policy_arn = aws_iam_policy.allow_write_listings.arn
 
     read_tokens_policy_arn = module.auth.allow_read_tokens_policy_arn
     write_tokens_policy_arn = module.auth.allow_write_tokens_policy_arn
