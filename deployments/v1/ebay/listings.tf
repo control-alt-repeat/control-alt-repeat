5a2c28fddfc830bce002b6b1667b018b14aa10e5
Matt Canty <matthewcanty@gmail.com>
2024-09-25 15:41:37 +0000

    TF FOO

--- deployments/v1/ebay/listings.tf
+++ deployments/v1/ebay/listings.tf
@@ -51,13 +51,13 @@ data "aws_iam_policy_document" "allow_listings_write" {
 }
 
 resource "aws_iam_policy" "allow_read_listings" {
-  name        = "AllowReadEbayListingPolicy"
+  name        = "AllowReadEbayListingPolicy2"
   description = "Policy for lambda reading listings"
   policy      = data.aws_iam_policy_document.allow_listings_read.json
 }
 
 resource "aws_iam_policy" "allow_write_listings" {
-  name        = "AllowWriteEbayListingPolicy"
+  name        = "AllowWriteEbayListingPolicy2"
   description = "Policy for lambda writing listings"
   policy      = data.aws_iam_policy_document.allow_listings_write.json
 }
