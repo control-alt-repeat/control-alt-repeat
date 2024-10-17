b2b4c334b9e8e7fa820161487d596226dd5abef0
Matt Canty <matt.canty@controlaltrepeat.net>
2024-10-17 13:22:18 +0100

    Put variables in right place

--- deployments/v1/ebay/lambda/notification-endpoint/variables.tf
+++ deployments/v1/ebay/lambda/notification-endpoint/variables.tf
@@ -1,11 +1,3 @@
-variable "read_warehouse_policy_arn" {
-  type = string
-}
-
-variable "write_warehouse_policy_arn" {
-  type = string
-}
-
 variable "allow_write_ebay_incoming_notifications_arn" {
   type = string
 }
--- deployments/v1/ebay/variables.tf
+++ deployments/v1/ebay/variables.tf
@@ -1,3 +1,11 @@
+variable "read_warehouse_policy_arn" {
+  type = string
+}
+
+variable "write_warehouse_policy_arn" {
+  type = string
+}
+
 variable "write_label_print_buffer_policy_arn" {
   type = string
 }
