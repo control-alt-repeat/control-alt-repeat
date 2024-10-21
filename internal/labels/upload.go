499dcab4310c8956146765b35bb328f0b2ffeafd
Matt Canty <matt.canty@controlaltrepeat.net>
2024-10-21 15:22:18 +0100

    Add labels.upload

---
+++ internal/labels/upload.go
@@ -0,0 +1,64 @@
+package labels
+
+import (
+	"bytes"
+	"fmt"
+	"mime/multipart"
+	"net/http"
+
+	"github.com/Control-Alt-Repeat/control-alt-repeat/internal/aws"
+)
+
+func UploadFileFromBytes(imageBytes []byte, filename string) error {
+	labelPrinterDomain, err := aws.GetParameterValue("eu-west-2", "/control_alt_repeat/ebay/live/label_printer/host_domain")
+	if err != nil {
+		return fmt.Errorf("error creating POST request: %v", err)
+	}
+
+	printURL := labelPrinterDomain + "/print"
+
+	fmt.Println("Sending print job to: ", printURL)
+	// Create a buffer and multipart writer
+	var body bytes.Buffer
+	writer := multipart.NewWriter(&body)
+
+	// Create form file field
+	formFile, err := writer.CreateFormFile("image", filename)
+	if err != nil {
+		return err
+	}
+
+	// Write the byte array to the form file
+	_, err = formFile.Write(imageBytes)
+	if err != nil {
+		return err
+	}
+
+	// Close the multipart writer
+	writer.Close()
+
+	// Create a request to upload the file
+	req, err := http.NewRequest("POST", printURL, &body)
+	if err != nil {
+		return err
+	}
+
+	// Set the content type to multipart
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check the response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("upload failed: %s", resp.Status)
+	}
+
+	fmt.Println("File uploaded successfully")
+	return nil
+}
