9e1e97b5a5c61ec7003d300899b9e878f70f744c
Matt Canty <matt.canty@controlaltrepeat.net>
2024-10-29 15:34:51 +0000

    Add global secondary indexes

--- deployments/v1/warehouse.tf
+++ deployments/v1/warehouse.tf
@@ -6,39 +6,59 @@ resource "aws_dynamodb_table" "warehouse" {
   name           = "control-alt-repeat-warehouse"
   billing_mode   = "PAY_PER_REQUEST"
 
+  hash_key = "ID"
+
   attribute {
     name = "ID"
     type = "S"
   }
 
-  hash_key = "ID"
-
   attribute {
-    name = "title"
+    name = "Shelf"
     type = "S"
   }
 
   attribute {
-    name = "shelf"
+    name = "Owner"
     type = "S"
   }
 
   attribute {
-    name = "owner"
-    type = "S"
-  }
-
-  attribute {
-    name = "createdAt"
+    name = "CreatedAt"
     type = "N"
   }
 
   attribute {
-    name = "updatedAt"
+    name = "UpdatedAt"
     type = "N"
+  }
+
+  global_secondary_index {
+    name            = "GSI_Shelf"
+    hash_key        = "Shelf"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "GSI_Owner"
+    hash_key        = "Owner"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name               = "CreatedAtIndex"
+    hash_key           = "ID"
+    range_key          = "CreatedAt"
+    projection_type    = "ALL"
+  }
+
+  global_secondary_index {
+    name               = "UpdatedAtIndex"
+    hash_key           = "ID"
+    range_key          = "UpdatedAt"
+    projection_type    = "ALL"
   }
 }
-
 
 data "aws_iam_policy_document" "allow_warehouse_read" {
   statement {
